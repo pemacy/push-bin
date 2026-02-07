d3c8eeeaf343c5a6c53e99b98b9bbadd19dab9f5
Preston Macy <preston.macy@gmail.com>
2026-02-06 23:53:41 -0500

    config update: synced typescript, removed unneeded type dependencies to optimize lsp

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,26 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/ws": {
+        target: "ws://localhost:3000",
+        ws: true,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/testSetup.ts",
-  }
-})
+  },
+});
